3b2f77fb23585f5414ecdf8f97ef5f4294bec1ac
harrish vijay <harrishvijay0708@gmail.com>
2024-10-11 12:07:10 +0530

    file save

--- demo-project/src/test/java/com/training/package1/TC010_Stringhandling.java
+++ demo-project/src/test/java/com/training/package1/TC010_Stringhandling.java
@@ -12,6 +12,12 @@ public class TC010_Stringhandling {
 		
 		System.out.println(str1);
 		System.out.println(str2);
+		
+		String str3 = "This is a beautiful day to start the work";
+		
+		System.out.println(str3.length());
+		System.out.println(str3.charAt(10));
+		System.out.println(str3.concat(", may this day give you delight"));
 	}
 
 }
